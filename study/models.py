8623caca115fe4ccafdceb8db7e5b573878dfb86
crndolo <ndoloraphael@gmail.com>
2018-11-20 11:33:28 +0300

    The country model has been created in this commit.

--- study/models.py
+++ study/models.py
@@ -7,3 +7,22 @@ class Category(models.Model):
 	created_on = models.DateTimeField(_('Created On'), auto_now_add=True)
 	category_name = models.CharField(_('Category Name'), max_length=255, blank=False, null=False)
 	created_by = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
+
+	class Meta:
+		verbose_name = "Category"
+		verbose_name_plural = "Categories"
+
+	def __str__(self):
+		return self.category_name
+
+class Country(models.Model):
+	updated_on = models.DateTimeField(_('Updated On'), auto_now=True)
+	created_on = models.DateTimeField(_('Created On'), auto_now_add=True)
+	country_name = models.CharField(_('Country Name'), max_length=255, blank=False, null=False)
+	continent_name = models.CharField(_('Continent Name'), max_length=255, blank=False, null=False)	
+	created_by = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
+
+	class Meta:
+		verbose_name = 'Country'
+		verbose_name_plural = 'Countries'
+
