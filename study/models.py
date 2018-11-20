7799d5e1b5bae0234f9bf814fdcb92d211c20b26
crndolo <ndoloraphael@gmail.com>
2018-11-20 11:27:21 +0300

    The category model has been created in this commit.

--- study/models.py
+++ study/models.py
@@ -1,4 +1,9 @@
 from django.db import models
-
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
+class Category(models.Model):	
+	updated_on = models.DateTimeField(_('Updated On'), auto_now=True)
+	created_on = models.DateTimeField(_('Created On'), auto_now_add=True)
+	category_name = models.CharField(_('Category Name'), max_length=255, blank=False, null=False)
+	created_by = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
