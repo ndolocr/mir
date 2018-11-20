d42f58362d1e5f9663f7c11b7775196c37dcf8a7
crndolo <ndoloraphael@gmail.com>
2018-11-20 12:40:18 +0300

    All models have been registered in the Admin module.

--- study/admin.py
+++ study/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from study.models import Tag
+from study.models import Study
+from study.models import Theme
+from study.models import Region
+from study.models import Country
+from study.models import Quality
+from study.models import Category
+from study.models import Resource
+from study.models import SubTheme
+from study.models import SubCategory
+
 # Register your models here.
+
+admin.site.register(Tag)
+admin.site.register(Study)
+admin.site.register(Theme)
+admin.site.register(Region)
+admin.site.register(Country)
+admin.site.register(Quality)
+admin.site.register(Category)
+admin.site.register(Resource)
+admin.site.register(SubTheme)
+admin.site.register(SubCategory)
