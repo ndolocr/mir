dd1e8f547007f5e279105ff3989e39acb402e3ab
crndolo <ndoloraphael@gmail.com>
2019-01-30 12:16:20 +0300

    Removed Tags and Quality as requested by client

--- study/admin.py
+++ study/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 #from import_export.admin import ImportExportModelAdmin
 
-from study.models import Tag
+#from study.models import Tag
 from study.models import Study
 from study.models import Theme
 from study.models import Region
 from study.models import Country
-from study.models import Quality
+#from study.models import Quality
 from study.models import Category
 from study.models import Resource
 from study.models import SubTheme
@@ -14,12 +14,12 @@ from study.models import SubCategory
 
 # Register your models here.
 
-admin.site.register(Tag)
+#admin.site.register(Tag)
 admin.site.register(Study)
 admin.site.register(Theme)
 admin.site.register(Region)
 admin.site.register(Country)
-admin.site.register(Quality)
+#admin.site.register(Quality)
 admin.site.register(Category)
 admin.site.register(Resource)
 admin.site.register(SubTheme)
